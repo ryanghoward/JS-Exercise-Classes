90f2ad4a9a37ed82ff01ec630562520238a863f6
Ryan Howard <ryanghoward@protonmail.com>
2021-09-16 13:05:36 -0700

    Task 1 Done

--- index.js
+++ index.js
@@ -42,8 +42,24 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age){
+  this.stomach = [];
+  this.name = name;
+  this.age = age;
+  }
+  eat(someFood){
+    if(this.stomach.length < 10){
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
+
 
 /*
   TASK 2
@@ -60,7 +76,25 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    if(distance <= driveMiles){
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance/milesPerGallon)
+     }else{
+       this.odometer = this.odometer + driveMiles;
+       this.tank = 0;
+       return `I ran out of fuel at ${this.odometer} miles!`
+     }
+  }
 }
 
 /*
